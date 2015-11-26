ae4c82f35c6e83e6d438f42c3f50801dad309a3a
huangyemin <huangyemin@3ikids.com>
2015-11-26 22:06:50 +0800

    can post a new blog with markdown

---
+++ filters.py
@@ -0,0 +1,9 @@
+# @app.template_filter('datetimeformat')
+# def format_datetime(value, format='medium'):
+#     if format == 'year':
+#         formatstr = "yyyy"
+#     elif format == 'date':
+#         formatstr = "yyyy-MM-dd"
+#     else:
+#         formatstr = "yyyy-MM-dd HH:mm:ss"
+#     return format_datetime(value, formatstr)
